4685ad15f694dd005604e6e82951f9b7a37deda1
datashaman <marlinf@datashaman.com>
2015-04-29 09:00:40 -0400

    WIP

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var app = express();
 
-var pkg = require('./pkg');
+var pkg = require('./index.js');
+
 pkg.boot(app, 'somesecret');
 
 app.post('/', pkg.router(function(req, res, event) {
-	switch (event) {
-	case 'ping':
-		res.send('Ping');
-	}
+    switch (event) {
+    case 'ping':
+        res.send('Ping');
+    }
 }));
 
 app.listen(80);
