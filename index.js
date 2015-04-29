9cc355e2536591bb59507bbf979ce4e3dfead5bf
datashaman <marlinf@datashaman.com>
2015-04-29 18:10:13 +0200

    Add the possibility of there being no signature, and enhance readme

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ module.exports = {
         app.use(bodyParser.raw({ type: 'application/json' }));
 
         app.use(function (req, res, next) {
+            var allowed = true;
             var body = req.body.toString('utf8');
-            var signature = 'sha1=' + Crypto.HMAC(Crypto.SHA1, body, secret, { asBytes: false });
 
-            if (signature === req.headers['x-hub-signature']) {
+            if (req.headers['x-hub-signature'] !== undefined) {
+                var signature = 'sha1=' + Crypto.HMAC(Crypto.SHA1, body, secret, { asBytes: false });
+                allowed = signature === req.headers['x-hub-signature'];
+            }
+
+            if (allowed) {
                 req.body = JSON.parse(body);
                 next();
             } else {
