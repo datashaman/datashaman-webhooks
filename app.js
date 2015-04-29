39f9fa3a8f555fb64bd88ba121e5710ad4098182
datashaman <marlinf@datashaman.com>
2015-04-29 15:15:57 +0200

    Fix few small issues, add log for demo app

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({ name: 'app' });
+
 var app = express();
 
 var pkg = require('./pkg');
 pkg.boot(app, 'somesecret');
 
 app.post('/', pkg.router(function(req, res, event) {
+    log.info(req.body);
+
 	switch (event) {
 	case 'ping':
 		res.send('Ping');
